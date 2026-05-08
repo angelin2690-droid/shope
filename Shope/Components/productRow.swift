17014a2060a275224d5f6ac8bad66b6512390f7e
Nalin <angelin2690@gmail.com>
2026-05-09 00:33:30 +0530

    commit 2

---
+++ Shope/Components/productRow.swift
@@ -0,0 +1,18 @@
+//
+//  productRow.swift
+//  Shope
+//
+//  Created by Nalin on 08/05/26.
+//
+
+import SwiftUI
+
+struct productRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    productRow()
+}
